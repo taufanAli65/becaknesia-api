a46aa84d5c49d2f72230d8f48dd24714a892315e
taufanAli65 <taufanali65@gmail.com>
2025-06-19 07:11:15 +0700

    feat: implement user model with roles and status enums

---
+++ src/models/users.ts
@@ -0,0 +1,53 @@
+import { Schema, model } from 'mongoose';
+
+export enum userRoles {
+    User = "user",
+    Admin = "admin",
+    Driver = "driver"
+}
+
+export enum userStatus {
+    Aktif = "aktif",
+    Nonaktif = "nonaktif"
+}
+
+export interface IUser extends Document {
+    name: string,
+    password: string,
+    email: string,
+    no_hp: string,
+    role: userRoles,
+    status: userStatus,
+    photoUrl: string
+}
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Email is invalid'],
+    },
+    no_hp: {type: String, required: true},
+    role: {
+        type: String,
+        enum: Object.values(userRoles),
+        required: true,
+        default: userRoles.User,
+    },
+    status: {
+        type: String,
+        enum: Object.values(userRoles),
+        required: true,
+        default: userStatus.Nonaktif,
+    },
+    photoUrl: {type: String, required: true}
+});
+
+const User = model<IUser>("Users", userSchema);
+
+export default User;
