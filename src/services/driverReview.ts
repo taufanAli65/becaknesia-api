d75d06f9b77ca7e35a293205f15cf54b4d6096c2
taufanAli65 <taufanali65@gmail.com>
2025-06-24 15:23:56 +0700

    feat: implement driver review services with CRUD operations and validation

---
+++ src/services/driverReview.ts
@@ -0,0 +1,55 @@
+import driverReview from "../models/driverReviews";
+import { AppError } from "../utils/appError";
+
+async function addReviewService(user_id: string, driver_id: string, stars: number, comment?: string) {
+    if (!user_id) throw AppError("User ID is required", 400);
+    if (!driver_id) throw AppError("Driver ID is required", 400);
+    if (stars > 5 || stars <= 0) throw AppError("Input stars range start from 1-5", 400);
+    const review = new driverReview({ user_id, driver_id, stars, comment });
+    await review.save();
+}
+
+async function getReviewsService(page: number = 1, limit: number = 10) {
+    const skip = (page - 1) * limit;
+    const reviews = await driverReview.find().skip(skip).limit(limit);
+    const total = await driverReview.countDocuments();
+    return {
+        data: reviews,
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit)
+    };
+}
+
+async function getReviewService(review_id: string) {
+    if (!review_id) throw AppError("Review ID is required", 400);
+    const review = await driverReview.findById(review_id);
+    if (!review) throw AppError("There is no review with such ID", 404);
+    return review;
+}
+
+async function updateReviewService(review_id: string, stars?: number, comment?: string) {
+    if(!review_id) throw AppError("Review ID is required", 400);
+    const updateFields: any = {};
+    if (stars !== undefined) updateFields.stars = stars;
+    if (comment !== undefined) updateFields.comment = comment;
+    const updatedReview = await driverReview.findByIdAndUpdate(
+        review_id,
+        { $set: updateFields },
+        { new: true, runValidators: true }
+    );
+    if (!updatedReview) {
+        throw AppError("Review not found", 404);
+    }
+    return updatedReview
+}
+
+async function deleteReviewService(review_id: string) {
+    if (!review_id) throw AppError("Review ID is required", 400);
+    const review = await driverReview.findByIdAndDelete(review_id);
+    if (!review) throw AppError("Review not found", 404);
+    return review;
+}
+
+export {addReviewService, getReviewsService, getReviewService, updateReviewService, deleteReviewService}
