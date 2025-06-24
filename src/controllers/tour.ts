083bde69de002c7905153e4459a73d157c53534a
taufanAli65 <taufanali65@gmail.com>
2025-06-24 08:49:46 +0700

    feat: add CRUD operations for tour packages with validation and error handling
    Description:
    
    Added create, read, update, and delete (CRUD) operations for tour packages in the service and controller layers.
    Integrated input validation using Zod schemas for all relevant endpoints.
    Implemented error handling for missing or invalid tour IDs and for cases where a tour package is not found.
    Ensured paginated retrieval of tour packages.
    All operations return consistent success and error responses using utility functions.

--- src/controllers/tour.ts
+++ src/controllers/tour.ts
@@ -1,14 +1,61 @@
 import { Request, Response, NextFunction } from "express";
-import { createNewTourPackageService } from "../services/tour";
+import { createNewTourPackageService, getTourPackagesService, getTourPackageService, updateTourPackageService, deleteTourPackageService } from "../services/tour";
 import { sendFail, sendSuccess } from "../utils/senResponse";
 import { validate } from "../utils/validate";
-import { createNewTourPackageSchema } from "../validators/tour";
+import { createNewTourPackageSchema, needTourIDSchema, updateTourPackageSchema } from "../validators/tour";
 
 export const createNewTourPackage = async(req: Request, res: Response, next: NextFunction) => {
     try {
         const {route_name, description, duration, distances, routes,prices} = validate(createNewTourPackageSchema, req.body);
         await createNewTourPackageService(route_name, description, duration, distances, routes,prices);
         return sendSuccess(res, 200, "New tour package is created successfully");
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        return sendFail(res, 400, errorMessage, error);
+    }
+}
+
+export const getTourPackages = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const page = parseInt(req.query.page as string) || 1; // default = 1;
+        const limit = parseInt(req.query.limit as string) || 10 // default = 10;
+        
+        const result = await getTourPackagesService(page, limit);
+        return sendSuccess(res, 200, "Tour packages fetched successfully", result);
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        return sendFail(res, 400, errorMessage, error);
+    }
+}
+
+export const getTourPackage = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {tourID} = validate(needTourIDSchema, req.params);
+        const tour = await getTourPackageService(tourID);
+        return sendSuccess(res, 200, `Tour package with ID: ${tourID} fetched successfully`, tour);
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        return sendFail(res, 400, errorMessage, error);
+    }
+}
+
+export const updateTourPackageController = async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const {tourID} = validate(needTourIDSchema, req.params);
+    const {route_name, description, duration, distances, routes, prices} = validate(updateTourPackageSchema, req.body);
+    const updatedTour = await updateTourPackageService(tourID, route_name, description, duration, distances, routes, prices);
+    return sendSuccess(res, 200, "Tour package updated successfully", updatedTour);
+  } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
+        return sendFail(res, 400, errorMessage, error);
+  }
+};
+
+export const deletedTourPackage = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {tourID} = validate(needTourIDSchema, req.params);
+        const deletedTourPackage = await deleteTourPackageService(tourID);
+        return sendSuccess(res, 200, "Tour package deleted successfully", deletedTourPackage);
     } catch (error) {
         const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
         return sendFail(res, 400, errorMessage, error);
--- src/validators/tour.ts
+++ src/validators/tour.ts
@@ -1,4 +1,9 @@
+import mongoose from "mongoose";
 import { z } from "zod";
+
+const objectIdSchema = z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
+    message: "Invalid ID, check again",
+});
 
 export const createNewTourPackageSchema = z.object({
     route_name: z.string().min(8, "Route name must be at least 8 characters long"),
@@ -7,4 +12,17 @@ export const createNewTourPackageSchema = z.object({
     distances: z.number(),
     routes: z.array(z.string()),
     prices: z.number()
-})
+});
+
+export const needTourIDSchema = z.object({
+    tourID: objectIdSchema
+});
+
+export const updateTourPackageSchema = z.object({
+  route_name: z.string().min(1, "Route name cannot be empty").optional(),
+  description: z.string().min(1, "Description cannot be empty").optional(),
+  duration: z.number().positive("Duration must be a positive number").optional(),
+  distances: z.number().positive("Distance must be a positive number").optional(),
+  routes: z.array(z.string()).optional(),
+  prices: z.number().positive("Price must be a positive number").optional()
+});
