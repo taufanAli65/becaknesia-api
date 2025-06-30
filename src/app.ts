95a68f49522718ce5ce9316acaaa27f672066167
taufanAli65 <taufanali65@gmail.com>
2025-06-30 09:22:46 +0700

    fix: remove serverless-http integration and restore route authentication for tour package retrieval

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,3 @@ connectDatabase().then(() => {
     console.error(error);
     process.exit(1);
 })
-
-import serverless from "serverless-http";
-module.exports = serverless(app);
--- src/routes/tour.ts
+++ src/routes/tour.ts
@@ -10,8 +10,8 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = Router();
 
 router.post("/", authenticated, authorize(userRoles.Admin), upload.single("photo"), (req: Request, res: Response, next: NextFunction) => {createNewTourPackage(req, res, next)});
-router.get("/", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => {getAllTourPackages(req, res, next)});
-router.get("/:tourID", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => {getTourPackage(req, res, next)});
+router.get("/", (req: Request, res: Response, next: NextFunction) => {getAllTourPackages(req, res, next)});
+router.get("/:tourID", (req: Request, res: Response, next: NextFunction) => {getTourPackage(req, res, next)});
 router.put("/:tourID", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => {updateTourPackage(req, res, next)});
 router.delete("/:tourID", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => {deletedTourPackage(req, res, next)});
 
