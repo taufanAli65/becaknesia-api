074793bc101a94f64d0cce1676cd8f19262773b4
taufanAli65 <taufanali65@gmail.com>
2025-07-01 20:01:45 +0700

    fix: restore order management routes for get, update, and delete operations

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -8,11 +8,11 @@ const router = Router();
 
 router.post("/", authenticated, (req: Request, res: Response, next: NextFunction) => { createOrder(req, res, next) });
 router.get("/", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => { getOrders(req, res, next) });
-router.get("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { getOrder(req, res, next) });
-router.put("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { updateOrder(req, res, next) });
-router.delete("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { deleteOrder(req, res, next) });
 router.get("/driver/accepted", authenticated, authorize(userRoles.Driver), (req: Request, res: Response, next: NextFunction) => { getAcceptedOrdersForDriver(req, res, next) });
 router.get("/user/:user_id/all", authenticated, (req: Request, res: Response, next: NextFunction) => { getOrdersByUserID(req, res, next) });
 router.get("/waiting", authenticated, authorize(userRoles.Admin), (req: Request, res: Response, next: NextFunction) => { getWaitingOrders(req, res, next) });
+router.get("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { getOrder(req, res, next) });
+router.put("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { updateOrder(req, res, next) });
+router.delete("/:order_id", authenticated, (req: Request, res: Response, next: NextFunction) => { deleteOrder(req, res, next) });
 
 export default router;
